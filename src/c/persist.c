39ab52d85abef797cc620f1337ae3fc919fd499a
Zipher <Zipher04@users.noreply.github.com>
2017-08-27 23:30:41 +0800

    Fix array out of boundary
    1. change strcat to strcpy for persist task title copy.

--- src/c/persist.c
+++ src/c/persist.c
@@ -33,6 +33,7 @@ int persist_get_task_count_key( int listId )
 int persist_write_list_count( int count )
 {
 	persist_write_int( PERSIST_LIST_COUNT, count );
+	LOG( "Persist: write %d: %d", PERSIST_LIST_COUNT, count );
 	return 0;
 }
 
@@ -45,29 +46,36 @@ int persist_write_task_count( int listId, int count )
 		return -1;
 	}
 	persist_write_int( key, count );
+	LOG( "Persist: write %d: %d", key, count );
 	return 0;
 }
 
 int persist_write_list( int listId, char* title )
 {
+	LOG( "Persist: write list called with %d: %s", PERSIST_LIST_COUNT+1+listId, title );
 	persist_write_string( PERSIST_LIST_COUNT+1+listId, title );
+	LOG( "Persist: write %d: %s", PERSIST_LIST_COUNT+1+listId, title );
 	return 0;
 }
 
 int persist_write_task( int listId, int taskId, char* title, int done )
 {
+	LOG( "Persist: write called with %d,%d: %s", listId, taskId, title );
 	int key = persist_get_task_count_key( listId );
 	if ( key <= 0 )
 	{
 		LOG("Persist: cannot found key for task count");
 		return -1;
 	}
+	LOG( "Persist: key to write: %d", key+taskId+1 );
 	int sizeString = strlen( title ) + 2;
 	char* string = malloc(sizeString);
 	string[0] = done ? '1' : '0';
-	strcat( string+1, title);
+	strcpy( string+1, title);
+	LOG( "Persist: string to write: %s", string );
 	persist_write_string( key+taskId+1, string );
 	free( string );
+	LOG( "Persist: write %d: %s", key+taskId+1, string );
 	return 0;
 }
 
@@ -114,7 +122,7 @@ int persist_read_task( int listId, int taskId, char* title, int sizeTitle, int *
 		LOG("Persist: reading not existing task");
 		return -1;
 	}
-	const int sizeString = sizeTitle + 1;
+	const int sizeString = sizeTitle + 2;
 	char *string = malloc( sizeString );
 	persist_read_string( key, string, sizeString );
 	*done = string[0] == '1';
