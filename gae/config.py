257e25c985aec0d9c99f0618e948377d8da7b2f4
Zipher <zipher04@yahoo.com.tw>
2017-09-24 20:49:16 +0800

    Change task server

--- gae/config.py
+++ gae/config.py
@@ -6,7 +6,7 @@ version = os.environ['CURRENT_VERSION_ID'].split('.')[0]
 # construct hostname for the current version
 # (use -dot- instead of . to avoid SSL problems)
 # see also: https://developers.google.com/appengine/kb/general#https
-host = "https://%s-dot-pebble-notes.appspot.com" % version
+host = "https://pebbletask-180910.appspot.com"
 
 # where user will be redirected after logging in with Google
 auth_redir_uri = host+"/auth/result"
