05e89708cd448aeff78a4ba3b4dc1be3ac72e1a2
Zipher <Zipher04@users.noreply.github.com>
2018-06-07 22:27:26 +0800

    Increase task id size
    to accommodate the change in Googlw

--- src/c/comm.c
+++ src/c/comm.c
@@ -211,7 +211,8 @@ void SentTaskToPhone( int taskIndex )
 	dict_write_tuplet(iter, &tUpdateTime );
 	app_message_outbox_send();
 	sb_printf_alloc(32);
-	snprintf( sb_printf_get(), 32, "Sending task %d...", taskIndex );
+	snprintf( sb_printf_get(), 32, "Sending task %d %6s", taskIndex, title );
+	//snprintf( sb_printf_get(), 32, "Sending task %d...", taskIndex );
 	sb_printf_update();
 }
 
